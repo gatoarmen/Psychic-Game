46723871108badb351aed9828dfeadf27619d3a7
esteban <esteban_armendariz@hotmail.com>
2018-03-04 15:02:51 -0600

    skeleton and instructions

---
+++ javascript/game.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+
+
+// Create an array of the words that you want to play with.
+
+const words = ['dog', 'latte', 'laptop'];
+
+// make the lines show up according to the lenght 
+
+
+
+
+
+
+// DOM event for people to type the letter 
