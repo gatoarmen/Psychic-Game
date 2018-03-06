26613e52bb9dc158bc125094181d476c5bbb102c
esteban <esteban_armendariz@hotmail.com>
2018-03-06 05:59:54 -0600

    stattements

--- javascript/game.js
+++ javascript/game.js
@@ -3,15 +3,70 @@
 
 
 
-// Create an array of the words that you want to play with.
-
-const words = ['dog', 'latte', 'laptop'];
-
-// make the lines show up according to the lenght 
+let wins = 0;
 
 
+
+let losses = 0;
+
+let remainingGuesses = 8;
+
+ 
+// Create an array of letters guess.
+
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// choose a random letter
+let randomLetter = letters[Math.floor(Math.random()*letters.length)];
+
+console.log(randomLetter)
 
 
 
 
 // DOM event for people to type the letter 
+
+ document.addEventListener("keydown", keyPress);
+
+ //compare letter guessed with random letter
+
+function keyPress() { 
+    
+        var userGuess = event.key;
+
+        if(userGuess === randomLetter){
+            wins++;
+        }else{
+            remainingGuesses--;
+        }
+    
+        if(guesses === 0){
+            losses++
+        }
+    
+        document.getElementById('wins').innerHTML = "Wins: " + wins;
+        document.getElementById('losses').innerHTML = "losses: " + losses;
+        document.getElementById('guesses').innerHTML = "Guesses left: " + remainingGuesses;
+    
+    }  // moved to below the innerHTML update
+    
+
+// compare the two letters
+
+
+
+
+
+
+
+
+// when they type the letter make the countdown go down 
+
+
+
+
+// make the letter appear on the screen 
+
+let lettersGuessed = [];
+
+console.log (lettersGuessed)
